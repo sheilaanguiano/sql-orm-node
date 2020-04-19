fccf95a273b18ed57b980ad0b95d3f0011c31b84
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-04-18 22:42:29 -0700

    Defined and Initialized model

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const sequelize = new Sequelize({
     storage: 'movies.db'
   });
 
+// Movie Model
+class Movie extends Sequelize.Model {}
+Movie.init({
+    title: Sequelize.STRING
+}, { sequelize });
+
+
 // async IIFE (Immediately Invoked Function EXpression)
 (async () => {
+    //Sync "Movies" table
+    await sequelize.sync({ force: true });
+
     try {
-        await sequelize.authenticate();
-        console.log('Connection to the database succesful!');
+        // await sequelize.authenticate();
+        // console.log('Connection to the database succesful!');
 
     } catch (error) {
         console.log('Error connecting to the database: ', error);
