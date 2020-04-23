bf84103d829f5d11bc2c0fe8eae4e5b7eed49052
Sheila Anguiano <sheila.anguiano@gmail.com>
2020-04-22 22:25:22 -0700

    Adding model data types

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const { Movie } = db.models;
 
   try {
     const movie = await Movie.create({
-      title: 'Silent Hill'
+      title: 'Silent Hill',
+      runtime: 125,
+      releaseDate: '2006-04-21',
+      isAvailableOnVHS: false,
     });
     console.log(movie.toJSON());
 
     const movie2 = await Movie.create({
-      title: 'Old boy'
+      title: 'Old boy',
+      runtime: 120,
+      releaseDate: '2003-11-21',
+      isAvailableOnVHS: false,
+
     });
     console.log(movie2.toJSON());
 
